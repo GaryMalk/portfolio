ac8d6d882f5823ff36d8d66e8cf67c5f34dc1734
Gary Malkasian <gary.malkasian@lwtech.edu>
2025-08-07 15:52:49 -0700

    submenu issues fixed, added js to hold subnav open, added favicon

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function showIntasc() {
+    var x = document.getElementById("subnav-content");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
