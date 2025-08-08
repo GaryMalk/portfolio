cee6396474cfd0176ee878f1f5cc997ab2b7506f
Gary Malkasian <GaryMalk@hotmail.com>
2025-08-07 22:02:30 -0700

    changed intasc pages to have subnav open by default, fixed spacing issues after h1 elements

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 function showIntasc() {
     var x = document.getElementById("subnav-content");
-    if (x.style.display === "block") {
+    var currentDisplay = window.getComputedStyle(x).display;
+
+    if (currentDisplay === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
