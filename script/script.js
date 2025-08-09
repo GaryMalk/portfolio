31d58e7c89d5bedf0f627d3b38d6ce4cc69c89d0
Gary Malkasian <GaryMalk@hotmail.com>
2025-08-08 19:05:18 -0700

    used script to resize school pages to fit school image without the empty <p></p> hack

--- script/script.js
+++ script/script.js
@@ -8,3 +8,23 @@ function showIntasc() {
         x.style.display = "block";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const section = document.querySelector("section.school");
+    const bgUrl = getComputedStyle(section).backgroundImage
+        .replace(/^url\(["']?/, '')
+        .replace(/["']?\)$/, '');
+
+    const img = new Image();
+    img.onload = function() {
+        // Width of section (in pixels)
+        const sectionWidth = section.offsetWidth;
+
+        // Scale the image height to match section width
+        const scale = sectionWidth / img.width;
+        const newHeight = img.height * scale;
+
+        section.style.height = newHeight + "px";
+    };
+    img.src = bgUrl;
+});
